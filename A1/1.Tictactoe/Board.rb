bd9d01de5bcbd3806cc41ee35e27b9f4a3c5bffa
Williams Estrada <we4841@gmail.com>
2018-09-21 20:15:37 -0500

    Code Review improve methos syntax and fix cleanBoard

--- A1/1.Tictactoe/Board.rb
+++ A1/1.Tictactoe/Board.rb
@@ -1,106 +1,90 @@
 class Board
-    attr_accessor :size, :board, :sumCol, :sumRow, :sumDiag
-    def initialize(size)
-        @size = size
-        @board = [[0,0,0],[0,0,0],[0,0,0]]
-        @sumCol = [0,0,0]
-        @sumRow = [0,0,0]
-        @sumDiag = [0,0]
+  attr_accessor :size, :board, :sumCol, :sumRow, :sumDiag
+  def initialize(size)
+    @size = size
+    @board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    @sumCol = [0, 0, 0]
+    @sumRow = [0, 0, 0]
+    @sumDiag = [0, 0]
+  end
+
+  # SET MARK ON BOARD and update sums
+  def setFigureOnBoard(posx, posy, value)
+    @board[posx][posy] = value
+    @sumCol[posx] += value
+    @sumRow[posy] += value
+    @sumDiag[0] += value if posx == posy
+    @sumDiag[1] += value if posx + posy == 2
+  end
+
+  def checkStatus
+    return true if checkCols || checkRows || checkDiags
+
+    false
+  end
+
+  def checkCols
+    @sumCol.each do |sum|
+      return true if sum.abs == 3
     end
-    # SET MARK ON BOARD and update sums
-    def setFigureOnBoard(posx, posy, value)
-        @board[posx][posy] = value
-        @sumCol[posx] += value
-        @sumRow[posy] += value
-        if posx == posy
-            @sumDiag[0] += value
-        end
-        if posx + posy == 2
-            @sumDiag[1] += value
-        end
+    false
+  end
+
+  def checkRows
+    @sumRow.each do |sum|
+      return true if sum.abs == 3
     end
-    
-    def checkStatus
-        if checkCols || checkRows || checkDiags
-            true
+    false
+  end
+
+  def checkDiags
+    @sumDiag.each do |sum|
+      return true if sum.abs == 3
+    end
+    false
+  end
+
+  def available(posx, posy)
+    (@board[posx][posy] == 0)
+  end
+
+  def printUIBoard
+    counter = 1
+    puts "\nBOARD SLOT POSITIONS TO SET MARKS ON\n-----------"
+    @board.each do |row|
+      row.each  do |_slot|
+        print " #{counter} |"
+        counter += 1
+      end
+      puts "\n-----------"
+    end
+  end
+
+  def printBoard
+    puts "\n-----------"
+    @board.each do |row|
+      row.each do |slot|
+        if slot == -1
+          print ' X |'
+        elsif slot == 1
+          print ' O |'
         else
-            false
+          print '   |'
         end
+      end
+      puts "\n-----------"
     end
+  end
 
-    def checkCols
-        gameWon = false
-        @sumCol.each do |sum|
-            if sum.abs == 3
-                gameWon = true
-                break
-            end
-        end
-        gameWon
+  def cleanBoard
+    0.upto(@size - 1) do |x|
+      0.upto(@size - 1) do |y|
+        @board[x][y] = 0
+      end
+      @sumCol[x] = 0
+      @sumRow[x] = 0
     end
-    def checkRows
-        gameWon = false
-        @sumRow.each do |sum|
-            if sum.abs == 3
-                gameWon = true
-                break
-            end
-        end
-        gameWon
-    end
-    def checkDiags
-        gameWon = false
-        @sumDiag.each do |sum|
-            if sum.abs == 3
-                gameWon = true
-                break
-            end
-        end
-        gameWon
-    end
-
-    def available(posx, posy)
-        (@board[posx][posy] == 0)
-    end
-
-    def printUIBoard
-        counter = 1
-        puts "\nBOARD SLOT POSITIONS TO SET MARKS ON\n-----------"
-        @board.each { |row|
-            row.each{ |slot|
-                print " #{counter} |"
-                counter += 1
-            }
-            puts "\n-----------"
-        }
-    end
-
-    def printBoard
-        puts "\n-----------"
-        @board.each { |row|
-            row.each{ |slot|
-                if slot == -1
-                    print " X |"
-                elsif slot == 1
-                    print " O |"
-                else
-                    print "   |"
-                end
-            }
-            puts "\n-----------"
-        }
-    end
-
-    def cleanBoard
-        for x in 0...@size do
-            for y in 0...@size do
-                @board[x][y] = 0
-            end
-            @sumCol[x] = 0
-            @sumRow[x] = 0
-        end
-        @sumDiag[0] = 0
-        @sumDiag[1] = 0
-    end
-
+    @sumDiag[0] = 0
+    @sumDiag[1] = 0
+  end
 end
